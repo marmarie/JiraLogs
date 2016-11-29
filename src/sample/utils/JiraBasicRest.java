9808c60a5e8eedd06727892c58db44ec36f32ef9
AliNaffaa <ali-naffaa.01@rambler.ru>
2016-11-29 09:50:50 +0200

    add new methods

--- src/sample/utils/JiraBasicRest.java
+++ src/sample/utils/JiraBasicRest.java
@@ -3,17 +3,29 @@ package sample.utils;
 import structure.JiraIssue;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Formatter;
+import java.util.HashMap;
 
 /**
  * Created by marie on 26.11.16.
  */
 public class JiraBasicRest {
+
+    public static final String JIRA_DATE_TIME_PATTERN = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+    public static final String DATE = "yyyy-MM-dd";
     public static void main(String args []) throws IOException {
         String basicIssue  = "AUT-10223";
-        int days = 50;
-        ArrayList<JiraIssue> issues = TestHttp.getLogWorkWithCredAndDays("",days);
-        JiraIssue issueToLog = TestHttp.getIssueListForLog(basicIssue,issues);
+        String cred = "YWxpLm5hZmZhYTo2e0swdSQyeXpa";
+        int days = 5;
+//        ArrayList<JiraIssue> issues = TestHttp.getLogWorkWithCredAndDays(cred,days);
+        HashMap<String,String> dateAndLogTime = TestHttp.getLogWork(cred,days);
+        JiraIssue issueToLog = TestHttp.getIssueListForLog(basicIssue,dateAndLogTime,days);
+        TestHttp.logWork(issueToLog,cred);
+
         System.out.println(issueToLog);
 
     }
--- src/sample/utils/TestHttp.java
+++ src/sample/utils/TestHttp.java
@@ -21,6 +21,9 @@ import java.util.Calendar;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 
+import static sample.utils.JiraBasicRest.DATE;
+import static sample.utils.JiraBasicRest.JIRA_DATE_TIME_PATTERN;
+
 /**
  * Created by ali.naffaa and mariya.azoyan 18.08.2016.
  */
@@ -50,15 +53,66 @@ public class TestHttp {
             jiraIssueArrayList.add(new JiraIssue(worklog.getKey(),date));
         }
         return jiraIssueArrayList;
-
     }
+
+
+    public static void logWork(JiraIssue issueToLog,String credentials) throws IOException {
+        ArrayList<JiraIssue> jiraIssueArrayList = new ArrayList<>();
+        HashMap<String,String> headersMap = new HashMap<>();
+        headersMap.put("Content-Type","application/json");
+        headersMap.put("Authorization", "Basic " + credentials);
+        String json = getPost(headersMap,"https://jira.ringcentral.com/rest/api/2/issue/"+issueToLog+"/worklog");
+
+        ObjectMapper mapper = new ObjectMapper();
+        Worklog[] w =  mapper.readValue(json, Result.class ).getWorklog();
+        for(Worklog worklog:w){
+            Entries [] entries = worklog.getEntries();
+            LinkedHashMap<String,String> date= new LinkedHashMap<>();
+            for(Entries entry: entries){
+                if(date.containsKey(entry.getStartDate())){
+                    String fullDate = String.valueOf(Long.valueOf(date.get(entry.getStartDate()))+Long.valueOf(entry.getTimeSpent()));
+                    date.put(entry.getStartDate(), fullDate);
+                }
+                else {
+                    date.put(entry.getStartDate(), entry.getTimeSpent());
+                }
+            }
+            jiraIssueArrayList.add(new JiraIssue(worklog.getKey(),date));
+        }
+    }
+
+
+    public static HashMap<String,String>  getLogWork(String credentials,int days) throws IOException {
+        HashMap<String,String> hashMap = new HashMap<>();
+        HashMap<String,String> headersMap = new HashMap<>();
+        headersMap.put("Content-Type","application/json");
+        headersMap.put("Authorization", "Basic " + credentials);
+        String json = getPost(headersMap,"https://jira.ringcentral.com/rest/timesheet-gadget/1.0/raw-timesheet.json?targetUser="+getUserName(credentials)+"&startDate="+getDate(days));
+
+        ObjectMapper mapper = new ObjectMapper();
+        Worklog[] w =  mapper.readValue(json, Result.class ).getWorklog();
+        for(Worklog worklog:w){
+            Entries [] entries = worklog.getEntries();
+            for(Entries entry: entries){
+                String date = getDate(entry.getStartDate());
+                if(hashMap.containsKey(date)){
+                    String fullDate = String.valueOf(Long.valueOf(hashMap.get(date))+Long.valueOf(entry.getTimeSpent()));
+                    hashMap.put(date, fullDate);
+                }
+                else {
+                    hashMap.put(date, entry.getTimeSpent());
+                }
+            }
+        }
+        return hashMap;
+    }
+
 
 
 
     private static String getPost(HashMap<String,String> headers,String url) throws IOException {
         StringBuilder sb = new StringBuilder();
         try(CloseableHttpClient client = HttpClients.createDefault()){
-
             HttpGet request = new HttpGet(url);
             for(String name:headers.keySet()){
                 request.setHeader(name,headers.get(name));
@@ -68,56 +122,62 @@ public class TestHttp {
                     BufferedReader reader =
                             new BufferedReader(new InputStreamReader(response.getEntity().getContent()), 65728);
                     String line = null;
-
                     while ((line = reader.readLine()) != null) {
                         sb.append(line);
                     }
                 }
                 catch (IOException e) { e.printStackTrace(); }
                 catch (Exception e) { e.printStackTrace(); }
-
-
             }
         }
-
         return sb.toString();
     }
 
-    public static JiraIssue getIssueListForLog(String basicIssueId,ArrayList<JiraIssue> alreadyLoggedList){
+    public static JiraIssue getIssueListForLog(String basicIssueId, HashMap<String, String> dateAndLogTime, int days) {
         JiraIssue needLogJira = new JiraIssue();
         needLogJira.setId(basicIssueId);
-        for(JiraIssue jiraIssue:alreadyLoggedList){
-            LinkedHashMap<String, String> logs = jiraIssue.getWorkLogs();
-            LinkedHashMap<String, String> needToLogMap = new LinkedHashMap<>();
-            for (String id:logs.keySet()){
-                if(!logs.get(id).equals("28800")){
-                    String leftToLog = String.valueOf(28800L - Long.parseLong(logs.get(id)));
-                    needLogJira.addWorkLog(id,leftToLog);
-                }
+        HashMap<String, String> data = getCalendarDays(days);
+        for (String id : data.keySet()) {
+            if (dateAndLogTime.keySet().contains(id) && !dateAndLogTime.get(id).equals("28800")) {
+                String leftToLog = String.valueOf(28800L - Long.parseLong(dateAndLogTime.get(id)));
+                needLogJira.addWorkLog(id, leftToLog);
+            } else {
+                needLogJira.addWorkLog(id, data.get(id));
             }
         }
         return needLogJira;
+    }
+
+    private static HashMap<String, String> getCalendarDays(int days) {
+        HashMap<String, String> data = new HashMap<>();
+        SimpleDateFormat dateTimeFormatter = new SimpleDateFormat(DATE);
+        for (int counter = 0; counter < days; counter++) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.DAY_OF_YEAR,-counter);
+            data.put(dateTimeFormatter.format(calendar.getTime()),"28800");
+        }
+        return data;
+    }
+
+    private static String getDate(String date){
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(Long.parseLong(date));
+        SimpleDateFormat dateTimeFormatter = new SimpleDateFormat(DATE);
+        return dateTimeFormatter.format(calendar.getTime());
     }
 
 
     private static String getDate(int days){
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DAY_OF_YEAR,-days);
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-        String date = format.format(calendar.getTime());
-        return date;
+        SimpleDateFormat format = new SimpleDateFormat(DATE);
+        return format.format(calendar.getTime());
     }
 
     private static String getUserName(String cred){
         byte[] decodedBytes = Base64.decodeBase64(cred);
         return new String(decodedBytes).split(":")[0];
     }
-
-    private static String transformData(){
-        //todo
-        return "";
-    }
-
 
 }
 

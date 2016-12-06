8048564562cd057adec59765206c724ecb73c541
AliNaffaa <ali-naffaa.01@rambler.ru>
2016-12-06 10:07:13 +0200

    update login

--- src/sample/LoginPage2.java
+++ src/sample/LoginPage2.java
@@ -4,6 +4,7 @@ package sample;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -58,12 +59,13 @@ public class LoginPage2 extends Application {
         grid.add(new Label("Password:"), 0, 1);
         grid.add(password, 1, 1);
         grid.add(saveCredentials, 1, 2);
-        grid.add(loginButton, 1, 2);
+        grid.add(loginButton, 1, 3);
+        grid.add(progressIndicator, 1, 4);
 
         // Enable/Disable login button depending on whether a username was entered.
         loginButton.setDisable(true);
 
-        root.getChildren().setAll(grid, progressIndicator);
+        root.getChildren().setAll(grid);
         // Do some validation (using the Java 8 lambda syntax).
         username.textProperty().addListener((observable, oldValue, newValue) ->
         {
@@ -82,34 +84,44 @@ public class LoginPage2 extends Application {
         }
 
         loginButton.setOnAction(event -> {
+            userPreferences.setUserName(username.getText());
+            userPreferences.setCredentials(encodeCredentials(username.getText() + ":" + password.getText()));
             Thread thread1 = new Thread(() -> {
-
-                userPreferences.setUserName(username.getText());
-                userPreferences.setCredentials(encodeCredentials(username.getText() + ":" + password.getText()));
+                progressIndicator.setVisible(true);
                 int code = 0;
                 try {
-                    System.out.println("Thread 2 ");
+                    System.out.println(Thread.currentThread());
                     code = basicAuthorization(userPreferences);
-                    progressIndicator.setVisible(true);
                 } catch (IOException e) {
                     e.printStackTrace();
                     System.out.println("ex");
                 }
                 if (code == 200) {
                     System.out.println("Code: " + code);
-                    if (saveCredentials.isSelected())
+                    if (saveCredentials.isSelected()){
                         try {
                             FileReader.saveUserPreferences(userPreferences);
-                            new LogJiraWorkUI().start(new Stage());
+
                         } catch (IOException e) {
                             e.printStackTrace();
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
-                } else new Alert(Alert.AlertType.INFORMATION, "Code " + String.valueOf(code)).show();
+                    }
+                    try {
+                        new LogJiraWorkUI().start(new Stage());
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        System.out.println("Error");
+                    }
+                } else{
+                    new Alert(Alert.AlertType.INFORMATION, "Code " + String.valueOf(code)).show();
+                }
 
             });
+            thread1.setDaemon(true);
             thread1.start();
+            System.out.println(Thread.currentThread());
         });
     }
 

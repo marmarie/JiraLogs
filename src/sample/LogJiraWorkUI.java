51a665d2d83df29d8751f687aa1f3aff0e4cae07
Mariya Azoyan <mariya.azoyan@ab-soft.net>
2016-12-26 18:21:57 +0200

    added tray

--- src/sample/LogJiraWorkUI.java
+++ src/sample/LogJiraWorkUI.java
@@ -1,6 +1,7 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Pos;
 import javafx.geometry.Side;
 import javafx.scene.Group;
@@ -13,18 +14,32 @@ import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.net.URL;
 import java.util.Locale;
 
 /**
  * Created by marie on 05.12.16.
  */
 public class LogJiraWorkUI extends Application {
-//    Stage primaryStage = new Stage();
+    Stage primaryStage ;
+private static final String iconImageLoc =
+        "http://icons.iconarchive.com/icons/scafer31000/bubble-circle-3/16/GameCenter-icon.png";
+
 
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
         Locale.setDefault(Locale.ENGLISH);
+
+        // instructs the javafx system not to exit implicitly when the last application window is shut.
+        Platform.setImplicitExit(false);
+
+        // sets up the tray icon (using awt code run on the swing thread).
+        javax.swing.SwingUtilities.invokeLater(this::addAppToTray);
+
         primaryStage.setTitle("Tabs");
         Group root = new Group();
         Scene scene = new Scene(root, 450, 250, Color.WHITE);
@@ -33,19 +48,10 @@ public class LogJiraWorkUI extends Application {
 
         BorderPane borderPane = new BorderPane();
 
-        Tab tabD = new Tab();
-        tabD.setClosable(false);
+        Tab tabD = getTabDays("Log days");
+        Tab tabS = getTabDays("Log Task");
+        Tab logAuto = getTabDays("Auto Log");
 
-        Tab tabS = new Tab();
-        tabS.setClosable(false);
-
-        Tab logAuto = new Tab();
-        logAuto.setClosable(false);
-
-        tabD.setGraphic(new Label("Log days"));
-        //tabS.setGraphic(new Clock());
-        tabS.setGraphic(new Label("Log task"));
-        logAuto.setGraphic(new Label("Auto log"));
         HBox hbox = new HBox();
 
         hbox.setAlignment(Pos.CENTER);
@@ -64,9 +70,90 @@ public class LogJiraWorkUI extends Application {
         borderPane.setLeft(tabPane);
         root.getChildren().add(borderPane);
         primaryStage.setScene(scene);
+
         primaryStage.show();
 
     }
+
+    private void addAppToTray() {
+        try {
+            // ensure awt toolkit is initialized.
+            java.awt.Toolkit.getDefaultToolkit();
+
+            // app requires system tray support, just exit if there is no support.
+            if (!java.awt.SystemTray.isSupported()) {
+                System.out.println("No system tray support, application exiting.");
+                Platform.exit();
+            }
+
+            // set up a system tray icon.
+            java.awt.SystemTray tray = java.awt.SystemTray.getSystemTray();
+            URL imageLoc = new URL(iconImageLoc);
+            java.awt.Image image = ImageIO.read(imageLoc);
+            java.awt.TrayIcon trayIcon = new java.awt.TrayIcon(image);
+
+            // if the user double-clicks on the tray icon, show the main app stage.
+            trayIcon.addActionListener(event -> Platform.runLater(this::showStage));
+
+            // if the user selects the default menu item (which includes the app name),
+            // show the main app stage.
+            java.awt.MenuItem logDaysTray = new java.awt.MenuItem("Log Days");
+            logDaysTray.addActionListener(event -> Platform.runLater(this::showStage));
+
+            // the convention for tray icons seems to be to set the default icon for opening
+            // the application stage in a bold font.
+            java.awt.Font defaultFont = java.awt.Font.decode(null);
+            java.awt.Font boldFont = defaultFont.deriveFont(java.awt.Font.BOLD);
+            logDaysTray.setFont(boldFont);
+
+            // to really exit the application, the user must go to the system tray icon
+            // and select the exit option, this will shutdown JavaFX and remove the
+            // tray icon (removing the tray icon will also shut down AWT).
+            java.awt.MenuItem exitItem = new java.awt.MenuItem("Exit");
+            exitItem.addActionListener(event -> {
+                Platform.exit();
+                tray.remove(trayIcon);
+            });
+
+            // setup the popup menu for the application.
+            final java.awt.PopupMenu popup = new java.awt.PopupMenu();
+            popup.add(logDaysTray);
+            popup.addSeparator();
+            popup.add(exitItem);
+            trayIcon.setPopupMenu(popup);
+
+
+            // add the application tray icon to the system tray.
+            tray.add(trayIcon);
+        } catch (java.awt.AWTException | IOException e) {
+            System.out.println("Unable to init system tray");
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+    private Tab getTabDays(String name){
+        Tab tabD = new Tab();
+        tabD.setClosable(false);
+        tabD.setGraphic(new Label(name));
+        return tabD;
+    }
+
+
+
+    /**
+     * Shows the application stage and ensures that it is brought ot the front of all stages.
+     */
+    private void showStage() {
+        if (this.primaryStage != null) {
+            this.primaryStage.show();
+            this.primaryStage.toFront();
+        }
+    }
+
+
 
     public static void main(String... args) {
         launch(args);
